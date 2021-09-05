fbaa14f358ef3286414bfdbee55c47a539d0a558
leeviko <lerppana0009@gmail.com>
2021-09-05 12:07:12 +0300

    useForm hook

--- client/src/components/Navbar/Search.js
+++ client/src/components/Navbar/Search.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import useForm from "../../hooks/useForm";
 
 const Search = () => {
+  const [values, handleChange] = useForm({ searchValue: "" })
+
   return (
     <form className="search-form">
-      <input className="search" placeholder="Search..." name="search" type="text" />
+      <input
+        className="search" 
+        placeholder="Search..." 
+        name="searchValue" 
+        type="text" 
+        value={values.searchValue}
+        onChange={handleChange}
+      />
     </form>      
   )
 }
