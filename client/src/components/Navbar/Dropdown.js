f17a826dfbf004b2449b0dc925258e80f8a675d8
leeviko <lerppana0009@gmail.com>
2021-09-06 20:34:53 +0300

    better dropdown

--- client/src/components/Navbar/Dropdown.js
+++ client/src/components/Navbar/Dropdown.js
@@ -6,6 +6,7 @@ const Dropdown = ({ show }) => {
     <div className={"dropdown " + (show ? "show" : "hide")}>
       <div className="dropdown-menu">
         <Link className="dropdown-link nav-item" to="/profile">Profile</Link>
+        <Link className="dropdown-link nav-item" to="/settings">Settings</Link>
         <Link className="dropdown-link nav-item" to="/logout">Log out</Link>
       </div>
     </div>
