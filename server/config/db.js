296c55a94db5e40db0295738e13c6210d033e4b9
leeviko <lerppana0009@gmail.com>
2021-09-07 14:01:21 +0300

    rename db

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   user: process.env.DB_USER,
   host: "localhost",
-  database: "journal-app",
+  database: "eveloper",
   password: process.env.DB_PASSWORD,
   port: 5432
 });
