bc9ace772175ab0a819d98535fc99f9d4960f3dc
leeviko <lerppana0009@gmail.com>
2021-09-16 13:31:42 +0300

    add if not exists

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE eveloper;
+CREATE DATABASE IF NOT EXISTS eveloper;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   uid varchar(255) NOT NULL PRIMARY KEY,
   email varchar(100) NOT NULL UNIQUE,
   name varchar(50) NOT NULL,
   password varchar(255) NOT NULL
 );
 
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
   bid varchar(255) NOT NULL PRIMARY KEY,
   uid varchar(255) REFERENCES users,
   title varchar(30) NOT NULL,
@@ -15,13 +15,13 @@ CREATE TABLE posts (
   tags text[],
 );
 
-CREATE TABLE votes_blogs (
+CREATE TABLE IF NOT EXISTS votes_blogs (
   vote_id varchar(255) NOT NULL PRIMARY KEY,
   uid varchar(255) REFERENCES users,
   bid VARCHAR(255) REFERENCES posts
 );
 
-CREATE TABLE favorited_blogs (
+CREATE TABLE IF NOT EXISTS favorited_blogs (
   favorite_id varchar(255) NOT NULL PRIMARY KEY,
   uid varchar(255) REFERENCES users,
   bid VARCHAR(255) REFERENCES posts
