8b73121cc7965a3b0c27527d2c00fbba1370df0e
leeviko <lerppana0009@gmail.com>
2021-10-08 13:58:08 +0300

    profile hook

---
+++ client/src/hooks/useProfile.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { PROFILE_ERROR, GET_PROFILE } from "../actions/types";
+import { useDispatch } from "react-redux";
+import { returnErrors } from "../actions/errorActions";
+
+const useProfile = (name) => {
+  const dispatch = useDispatch();
+  const [profile, setProfile] = useState(null);
+
+  const userProfile = () => {
+    const headers = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  
+
+    axios.get(`/api/users/profile/${name}`, headers)
+      .then((res) => {
+        dispatch({ type: GET_PROFILE })
+        setProfile(res.data)
+      })
+      .catch((err) => {
+        dispatch(returnErrors(err.response.data, err.response.status, "PROFILE_ERROR"));
+        dispatch({
+          type: PROFILE_ERROR
+        })
+      })
+
+    return profile;
+  }
+
+  useEffect(() => {
+    userProfile()
+  }, [])
+
+
+  return profile;
+}
+
+export default useProfile;
