18403ef01c3bbd905b94cc9ab615fba326361e4d
leeviko <leevikotilaine@gmail.com>
2021-10-13 12:57:27 +0300

    update database.sql

--- server/database.sql
+++ server/database.sql
@@ -3,8 +3,10 @@ CREATE DATABASE IF NOT EXISTS eveloper;
 CREATE TABLE IF NOT EXISTS users (
   uid varchar(255) NOT NULL PRIMARY KEY,
   email varchar(100) NOT NULL UNIQUE,
-  name varchar(50) NOT NULL,
-  password varchar(255) NOT NULL
+  name varchar(50) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  description varchar(255) NOT NULL DEFAULT 'There is nothing here',
+  createdat varchar(255) NOT NULL DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE IF NOT EXISTS posts (
@@ -12,7 +14,8 @@ CREATE TABLE IF NOT EXISTS posts (
   uid varchar(255) REFERENCES users,
   title varchar(30) NOT NULL,
   content text NOT NULL,
-  tags text[],
+  createdat DATE NOT NULL DEFAULT now(),
+  tags text[]
 );
 
 CREATE TABLE IF NOT EXISTS post_likes (
@@ -31,5 +34,13 @@ CREATE TABLE IF NOT EXISTS post_comments (
   comment_id varchar(255) primary key,
   uid varchar(255) REFERENCES users,
   bid varchar(255) REFERENCES posts,
-  comment varchar(255) not null
+  comment varchar(255) not null,
+  createdat date NOT NULL DEFAULT CURRENT_DATE,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_follows (
+  follow_id varchar(255) primary key,
+  follower_id varchar(255) REFERENCES users(uid),
+  followed_id varchar(255) REFERENCES users(uid)
 );
